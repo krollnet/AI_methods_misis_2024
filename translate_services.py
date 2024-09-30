8dc26f3a7c9ee3256bc22feea0b8ffd0d6315057
Полина Фонова <fpv-vol@mail.ru>
2024-09-30 15:34:40 +0300

    Changed constant names

--- translate_services.py
+++ translate_services.py
@@ -4,11 +4,11 @@ import json
 # Чтение API-ключа из файла config.json
 with open('config.json') as config_file:
     config = json.load(config_file)
-    api_key = config['api_key']
+    API_KEY = config['api_key']
 
 # Функция, работающая с Google Translate API
 def google_translate(text, source_language="ru", target_language="en"):
-    url = "https://google-translator9.p.rapidapi.com/v2"
+    URL = "https://google-translator9.p.rapidapi.com/v2"
     payload = {
 	    "q": text,
 	    "source": source_language,
@@ -16,27 +16,27 @@ def google_translate(text, source_language="ru", target_language="en"):
 	    "format": "text"
     }
     headers = {
-        "x-rapidapi-key": api_key,
+        "x-rapidapi-key": API_KEY,
         "x-rapidapi-host": "google-translator9.p.rapidapi.com",
         "Content-Type": "application/json"
     }
-    response = requests.post(url, json=payload, headers=headers)
+    response = requests.post(URL, json=payload, headers=headers)
     return response.json()["data"]["translations"][0]["translatedText"]
 
 # Функция, работающая с Albit Translator API
 def albit_translator(text, from_language="ru", to_language="en"):
-    url = "https://aibit-translator.p.rapidapi.com/api/v1/translator/text"
+    URL = "https://aibit-translator.p.rapidapi.com/api/v1/translator/text"
     payload = {
         "from": from_language,
         "to": to_language,
         "text": text
     }
     headers = {
-        "x-rapidapi-key": api_key,
+        "x-rapidapi-key": API_KEY,
         "x-rapidapi-host": "aibit-translator.p.rapidapi.com",
         "Content-Type": "application/json"
     }
-    response = requests.post(url, json=payload, headers=headers)
+    response = requests.post(URL, json=payload, headers=headers)
     return response.json()["trans"]
 
 
